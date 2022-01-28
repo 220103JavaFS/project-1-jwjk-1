e034f90c8d8a0e48fb68543bc03f392e71603fdb
jkim0237 <ppiddagi2000@gmail.com>
2022-01-28 12:40:22 -0500

    added some Userdao

--- src/main/java/com/revature/repos/UserDAO.java
+++ src/main/java/com/revature/repos/UserDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserDAO {
 
     List<User> findAllUsers();
+    User findUserByUserName(String userName);
     boolean updateUsername();
     boolean updatePassword();
     boolean updateEmail();
--- src/main/java/com/revature/repos/UserDAOImpl.java
+++ src/main/java/com/revature/repos/UserDAOImpl.java
@@ -1,7 +1,10 @@
 package com.revature.repos;
 
 import com.revature.models.User;
+import com.revature.utils.ConnectionUtil;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO{
@@ -9,6 +12,55 @@ public class UserDAOImpl implements UserDAO{
 
     @Override
     public List<User> findAllUsers() {
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "SELECT * FROM users;";
+
+            Statement statement = conn.createStatement();
+
+            ResultSet rs = statement.executeQuery(sql);
+
+            List<User> list = new ArrayList<>();
+
+            while (rs.next()) {
+                User user = new User;
+                user.setUserID(rs.getInt("user_id"));
+                user.setUserName(rs.getString("username"));
+                user.setPassword(rs.getString("passwords"));
+                user.setUserFirstName(rs.getString("user_first_name"));
+                user.setUserLastName(rs.getString("user_last_name"));
+                user.setUserEmail(rs.getString("user_email"));
+                user.setUserRoleId(rs.getInt("user_role_id"));
+                list.add(user);
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<User>();
+    }
+
+    @Override
+    public User findUserByUserName(String userName) {
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "SELECT * FROM users WHERE username = ?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, userName );
+            ResultSet rs = ps.executeQuery();
+
+            User user = new User();
+            while (rs.next()) {
+                user.setUserID(rs.getInt("user_id"));
+                user.setUserName(rs.getString("username"));
+                user.setPassword(rs.getString("passwords"));
+                user.setUserFirstName(rs.getString("user_first_name"));
+                user.setUserLastName(rs.getString("user_last_name"));
+                user.setUserEmail(rs.getString("user_email"));
+                user.setUserRoleId(rs.getInt("user_role_id"));
+            }
+            return user;
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
